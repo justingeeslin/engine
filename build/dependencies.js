c56eaea9a706997d1fe616f158f0936b71480534
Justin Geeslin <justin@geesl.in>
2017-01-10 17:48:10 -0600

    Creating a tasks to create a version of playcanvas with ammo.js, a dependency, included in the file.

---
+++ build/dependencies.js
@@ -0,0 +1,1 @@
+const ammo = require('ammo.js')
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 var gulp = require('gulp')
 var shell = require('gulp-shell')
+var concat = require('gulp-concat');
 
 gulp.task('build', shell.task([
   'cd build && node build.js -l 0 -o output/playcanvas-latest.js',
   'cd build && node build.js -l 1 -o output/playcanvas-latest.min.js',
   'cd build && node build.js -l 0 -d -o output/playcanvas-latest.dbg.js'
 ]))
+
+// Tasks to bundle ammo and playcanvas
+gulp.task('js', function() {
+  return gulp.src([
+    'build/dependencies.js',
+    'build/output/playcanvas-latest.js'
+  ])
+  .pipe(concat('ammo-playcanvas.js'))
+	.pipe(gulp.dest('build/output'))
+});
